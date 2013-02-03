69d5d7446096f751b776a5c46c0b3343f09f0ccd
Rapha <raphamaiostre@gmail.com>
2013-02-03 21:51:34 +0100

    player can move

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import sys
 
 white = pygame.Color("white")
 
@@ -16,15 +17,27 @@ from ddm import Level, Player
 lev = Level(size_max=21)
 pl = Player()
 x_min, x_max, y_min, y_max = lev.getBounds()
-for x, y in lev.rooms:
-    print(x, y)
-    DISPLAYSURF.blit(img, (x * img_width - img_width * x_min, y * img_height - img_height * y_min))
-DISPLAYSURF.blit(img_player, (pl.xy[0] * img_width - img_width * x_min, pl.xy[1] * img_height - img_height * y_min))
 lev.draw()
 
 while True: # main game loop
+    for x, y in lev.rooms:
+        DISPLAYSURF.blit(img, (x * img_width - img_width * x_min, y * img_height - img_height * y_min))
+    DISPLAYSURF.blit(img_player, (pl.xy[0] * img_width - img_width * x_min, pl.xy[1] * img_height - img_height * y_min))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == KEYDOWN:
+            x, y = pl.xy
+            if event.key == K_LEFT:
+                x -= 1
+            elif event.key == K_RIGHT:
+                x += 1
+            elif event.key == K_UP:
+                y -= 1
+            elif event.key == K_DOWN:
+                y += 1
+            if lev.isRoom(x, y):
+                pl.xy = (x, y)
     pygame.display.update()
